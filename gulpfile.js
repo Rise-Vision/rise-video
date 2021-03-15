d411f366e343e28035d1b4ffd1985b448e78951e
Ezequiel Conte <ezequielc@gmail.com>
2021-03-15 15:24:03 -0300

    Add new dist file that points to polymer-bundle.js (#71)
    * Update rise-common-element version
    
    * Add dependencies to bundle.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,20 @@
 const concat = require("gulp-concat");
 const gulp = require("gulp");
 
-const bundle = "dist/rise-video.js";
+const bundles = [ 
+	"dist/rise-video.js", 
+	"dist/rise-video-bundle.min.js"
+];
 const dependencies = [
   "node_modules/video.js/dist/video.min.js",
   "node_modules/videojs-playlist/dist/videojs-playlist.min.js"
 ];
 
-gulp.task( "default", () => {
-  return gulp.src( dependencies.concat( bundle ) )
-    .pipe( concat( bundle ) )
-    .pipe( gulp.dest( "." ) );
+gulp.task( "default", (done) => {
+  bundles.map(function(file) {
+    return gulp.src( dependencies.concat( file ) )
+      .pipe( concat( file ) )
+      .pipe( gulp.dest( "." ) );
+  });
+  done();
 });
